20f908a93f95d373d0c8619e1b3a668390e1ef9f
Edson Junior <49772981+projetos-junior@users.noreply.github.com>
2024-06-13 20:39:56 -0300

    :tada: Commit inicial

---
+++ aula2/aula2.py
@@ -0,0 +1,12 @@
+
+""""
+Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10
+"""
+
+nome =  " PAULA MARTINS"
+mes = "JANEIRO "
+valor = 500
+desconto = 10
+cupom = "PAULAÉ10"
+
+print(f"Olá,{nome}. Em {mes}você realizou uma compra no valor de R${valor} e ganhou um desconto de %{desconto} em sua proxima compra. Use o cumpom {cupom}")
